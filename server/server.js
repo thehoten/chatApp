1d815d10c128f739be5eb716b022dc14405683e8
thehoten <michaelhutton01@hotmail.com>
2019-01-06 16:51:45 +0000

    create generate mesage utility

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path=require('path');
 const http=require('http');
 const express=require('express');
 const socketIO=require('socket.io');
+const {generateMessage}=require('./utils/message');
 
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT||3000;
@@ -18,6 +19,10 @@ io.on('connection',(socket)=>{
     console.log('client disconnected');
   });
 
+  socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
+
+  socket.broadcast.emit('newMessage',generateMessage('Admin','A new user joined'));
+
 
 
   // socket.emit('newMessage',{
@@ -30,11 +35,12 @@ io.on('connection',(socket)=>{
 
   socket.on('createMessage',(message)=>{
     console.log('createMessage',message);
-    io.emit('newMessage',{
-      from:message.from,
-      text:message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage',generateMessage(message.from,message.text));
+    // socket.broadcast.emit('newMessage',{
+    //   from:message.from,
+    //   text:message.text,
+    //   createdAt: new Date().getTime()
+    // });
 
   });
 
