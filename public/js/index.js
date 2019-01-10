2ba17136d75489b1c9f6de9910ebe0291a554e04
thehoten <michaelhutton01@hotmail.com>
2019-01-10 11:29:41 +0000

    scroll to bottom if user is close to buttom

--- public/js/index.js
+++ public/js/index.js
@@ -37,6 +37,8 @@ socket.on('newMessage',function(message){
   });
 
   $('#messages').append(html);
+
+  scrollToBottom();
 });
 
 
@@ -93,4 +95,22 @@ socket.on('newLocationMessage',function(message){
 
   $('#messages').append(html);
 
+  scrollToBottom();
+
 });
+
+function scrollToBottom(){
+  var messages=$('#messages');
+  var newMessage=messages.children('li:last-child');
+
+  var clientHeight=messages.prop('clientHeight');
+  var scrollTop=messages.prop('scrollTop');
+  var scrollHeight=messages.prop('scrollHeight');
+  var newMessageHeight=newMessage.innerHeight();
+  var lastMessageHeight=newMessage.prev().innerHeight();
+
+  if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight){
+    messages.scrollTop(scrollHeight);
+    console.log('hey');
+  }
+}
